95563c7ee86783ee8fcd5f22bf2a8cea03fdec90
László Gábor <lacigabi@windowslive.com>
2018-07-09 14:09:40 +0200

    Footer

---
+++ resources/views/content/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="footer">
+    <ul>
+        <li>
+            <a href="index.html">Home</a>
+        </li>
+        <li>
+            <a href="about.html">About</a>
+        </li>
+        <li>
+            <a href="news.html">News</a>
+        </li>
+        <li>
+            <a href="contact.html">Contact</a>
+        </li>
+    </ul>
+    <p>
+        &#169; Copyright &#169; 2023.Company name all rights reserved
+    </p>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -37,25 +37,7 @@
 				</p>
 			</div>
 		</div>
-		<div class="footer">
-			<ul>
-				<li>
-					<a href="index.html">Home</a>
-				</li>
-				<li>
-					<a href="about.html">About</a>
-				</li>
-				<li>
-					<a href="news.html">News</a>
-				</li>
-				<li>
-					<a href="contact.html">Contact</a>
-				</li>
-			</ul>
-			<p>
-				&#169; Copyright &#169; 2023.Company name all rights reserved
-			</p>
-		</div>
+		@include('content.footer')
 	</div>
 </body>
 </html>
